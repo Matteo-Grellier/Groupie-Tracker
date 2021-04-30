882a5ecbfeeaa9be4ff65b50473001e5af61cad8
Matteo-Grellier <matteo-g@orange.fr>
2021-04-30 08:29:22 +0200

    Update server.go + groupes.html + navbar.html + groupes.go + requestAPI.go
    Can display groupes ID in html page

--- server.go
+++ server.go
@@ -1,19 +1,17 @@
 package main
 
 import (
-	api "Groupie-Tracker/src/API"
+	api "Groupie-Tracker/src/api"
 	artist "Groupie-Tracker/src/artist"
 	carte "Groupie-Tracker/src/carte"
-	groupe "Groupie-Tracker/src/groupes"
 	home "Groupie-Tracker/src/home"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/api", api.LoadAPI)
 	http.HandleFunc("/", home.Home)
 	http.HandleFunc("/artist", artist.Artists)
-	http.HandleFunc("/groupes", groupe.Groupes)
+	http.HandleFunc("/groupes", api.GroupePage)
 	http.HandleFunc("/carte", carte.Carte)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.ListenAndServe(":8080", nil)
--- src/API/requestAPI.go
+++ src/API/requestAPI.go
@@ -1,23 +1,57 @@
 package LoadAPI
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"text/template"
 )
 
-func LoadAPI(w http.ResponseWriter, r *http.Request) {
-	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+const JSON string = "https://groupietrackers.herokuapp.com/api/artists"
 
+type Groupe struct {
+	ID    int    `json:"id"`
+	Image string `json:"image"`
+	Name  string `json:"name"`
+}
+
+type PageDataGroupe struct {
+	Groupes []Groupe
+}
+
+func GroupePage(w http.ResponseWriter, r *http.Request) {
+
+	groupes := GetAPI()
+	pageGroupes := PageDataGroupe{Groupes: groupes}
+
+	ts, err := template.ParseFiles("./HTML/layout.html", "./HTML/groupes.html", "./HTML/navbar.html")
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error - PARSING", 500)
+		return
 	}
-
-	respData, _ := ioutil.ReadAll(resp.Body)
-
+	err = ts.Execute(w, pageGroupes)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error - EXECUTE", 500)
 	}
+}
 
-	w.Write(respData) /// permet de l'ecrire dans le header
+func GetAPI() []Groupe {
+
+	var GroupeList []Groupe
+	res, err := http.Get(JSON)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	json.Unmarshal(data, &GroupeList)
+	return GroupeList
 }
