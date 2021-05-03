75f21925db32bc7cfd1126fa43ef003a59898b89
yann Bourgeron <75064765+Chakyu23@users.noreply.github.com>
2021-05-03 09:14:51 +0200

    add listof concert

--- JS/carte.js
+++ JS/carte.js
@@ -6,5 +6,14 @@ function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 47.20565350041161, lng: -1.5389924790850191},
     zoom: 8,
+    concertlist()
   });
+}
+
+function concertlist() {
+  
+}
+
+function lien() {
+
 }
