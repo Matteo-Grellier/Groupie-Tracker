477e33acdbb59310663517d430ad5907b2a3895f
Matteo-Grellier <matteo-g@orange.fr>
2021-05-03 16:11:53 +0200

    Delete artist.html + artist.go
    Update navbar.html + server.go
    
    Delete artist page because we dont need it

--- server.go
+++ server.go
@@ -3,7 +3,6 @@ package main
 import (
 	APIgroupe "Groupie-Tracker/src/APIgroupe"
 	APIcarte "Groupie-Tracker/src/APIlocations"
-	artist "Groupie-Tracker/src/artist"
 	home "Groupie-Tracker/src/home"
 	"net/http"
 )
@@ -11,7 +10,6 @@ import (
 func main() {
 
 	http.HandleFunc("/", home.Home)
-	http.HandleFunc("/artist", artist.Artists)
 	http.HandleFunc("/groupes", APIgroupe.GroupePage)
 	http.HandleFunc("/carte", APIcarte.CartePage)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
