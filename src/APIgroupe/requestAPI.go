4a938d5cf24218f5c59249c47d9b977cbdc2df74
nsegur66 <nico.segura44@gmail.com>
2021-05-04 11:07:43 +0200

    .

--- src/APIgroupe/requestAPI.go
+++ src/APIgroupe/requestAPI.go
@@ -12,10 +12,13 @@ import (
 const JSONartist string = "https://groupietrackers.herokuapp.com/api/artists"
 
 type Groupe struct {
-	ID           int    `json:"id"`
-	Image        string `json:"image"`
-	Name         string `json:"name"`
-	CreationDate int    `json:"creationDate"`
+	ID           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	CreationDate int      `json:"creationDate"`
+	Members      []string `json:"members"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	ConcertDates string   `json:"concertDates"`
 }
 
 type PageDataGroupe struct {
