f06225b44291367bfa7200bb3025bca839670747
LJONES <luke.jones@ynov.com>
2021-05-05 10:22:38 +0200

    js et go

--- static/JS/main.js
+++ static/JS/main.js
@@ -1,80 +1,61 @@
+const charactersList = document.getElementById('charactersList');
+const searchinput = document.getElementById('searchInput');
+let persons = [];
 
-export function display() {
-    fetch('/api')
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            sessionStorage.setItem('datasave', data)
-            document.getElementById("nuke").onclick = function () {
-                document.location.reload()
-                sessionStorage.clear()
-            }
-            document.getElementById("srch").onclick = function () {
-                srch(data)
-            };
-            appendData(data);
+searchinput.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
 
-        })
+    const filteredCharacters = persons.filter((character) => {
+        return (
+            character.name.toLowerCase().includes(searchString) ||
+            character.house.toLowerCase().includes(searchString)
+        );
+    });
+    displayCharacters(filteredCharacters);
+});
 
-    function appendData(heroes) {
-        let table = document.getElementById("tab");
-        let row = table.insertRow();
-        var row1 = table.insertRow();
-        // Mise en place du nombre de page par défaut qui est de 20
-        for (let i = 0; i < heroes.length; i++) { // on va parcourir le tableau d'hero
-            // on creer une variable qui va prendre pour information tout ce qui ce trouve dans l'id "tab" // on creer un tableau
-            let cell = row.insertCell();
-
-            console.log("tourne");
-            if (i === 10) {
-                cell = row1.insertCell();
-                let icon = row.insertCell();
-                cell.innerHTML = heroes[i].name;
-                let imageName = ["background-image:url('", heroes[i].image, "')"]; // on ajoute les icones des personnages dans le style de la page
-                imageName = imageName.join("");
-                icon.style = imageName;
-
-            }
-
-            cell.innerHTML = heroes[i].name;
-        }
-
+const loadCharacters = async () => {
+    try {
+        const res = await fetch('https://groupietrackers.herokuapp.com/api/artists');
+        persons = await res.json();
+        displayCharacters(persons);
+    } catch (err) {
+        console.error(err);
     }
-}
-export function srch(heroes) {
-    let research = document.getElementById("searchInput").value;
-    let search = new RegExp(research);
-    let finded = []
-    let valid = []
-    console.log(research)
-    for (let i = 0; i < heroes.length; i++) {
-        valid.push(false)
-        for (const property in heroes[i]) {
-            if (typeof heroes[i][property] == 'object') {
-                for (const subproperty in heroes[i][property]) {
-                    if (typeof heroes[i][property][subproperty] == 'object') {
-                        for (const sublist in heroes[i][property][subproperty]) {
-                            if (search.test(heroes[i][property][subproperty][sublist]) == true) {
-                                valid[i] = true
-                            }
-                        }
-                    } else {
-                        if (search.test(heroes[i][property][subproperty]) == true) {
-                            valid[i] = true
-                        }
-                    }
-                }
-            } else {
-                if (search.test(heroes[i][property]) == true) {
-                    valid[i] = true
-                }
-            }
-        }
-        if (valid[i] == true) {
-            finded.push(heroes[i])
-        }
+};
+
+const displayCharacters = (characters) => {
+    const htmlString = characters
+        .map((character) => {
+            return `
+            <li class="character">
+                <h2>${character.name}</h2>
+                <p>House: ${character.house}</p>
+                <img src="${character.image}"></img>
+            </li>
+        `;
+        })
+        .join('');
+    charactersList.innerHTML = htmlString;
+};
+
+searchinput.addEventListener('keyup', function(){
+    const input = searchinput.value;
+
+    const result = persons.filter(item => item.name.toLowerCase().includes(input.toLowerCase()));
+
+    let suggestion = '';
+
+    if(input !=''){
+    result.forEach(resultItem => 
+            suggestion +=`
+                <div class="suggestion">${resultItem.name}</div>
+            `
+        )
     }
-    finded = JSON.stringify(finded)
-    sessionStorage.setItem("data", finded)
-}
+    document.getElementById("suggestions").innerHTML = suggestion;
+
+})
+
+console.log(persons)
+loadCharacters();
